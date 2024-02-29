2f12ac60116742be331ee469fd0294df5d69aa87
Juan Hamilton <hedwards507@gmail.com>
2024-02-29 14:30:37 -0500

    Update CV & links

--- components/pages/home/hero.tsx
+++ components/pages/home/hero.tsx
@@ -1,9 +1,9 @@
-import { Box, Stack, Typography } from '@mui/material'
-import { useTranslation } from 'next-i18next'
-import { useRouter } from 'next/router'
-import React from 'react'
-import ElevatedButton from '../../button/elevated-button'
-import TextButton from '../../button/text-button'
+import { Box, Stack, Typography } from '@mui/material';
+import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
+import React from 'react';
+import ElevatedButton from '../../button/elevated-button';
+import TextButton from '../../button/text-button';
 
 const PatternRingsRight = () => {
   return (
@@ -20,8 +20,8 @@ const PatternRingsRight = () => {
         zIndex: 0,
       }}
     />
-  )
-}
+  );
+};
 
 const PatternRingsLeft = () => {
   return (
@@ -38,19 +38,21 @@ const PatternRingsLeft = () => {
         zIndex: 0,
       }}
     />
-  )
-}
+  );
+};
 
 const Hero = React.forwardRef((_, ref) => {
-  const { t } = useTranslation()
-  const { locale } = useRouter()
+  const { t } = useTranslation();
+  const { locale } = useRouter();
 
   const resumeLink =
-    locale === 'en' ? '/resume-juan-hamilton.pdf' : '/cv-juan-hamilton.pdf'
+    locale === 'en'
+      ? '/cv-juan-hamilton.pdf'
+      : '/hoja-de-vida-juan-hamilton.pdf';
 
   return (
     <Stack
-      component='header'
+      component="header"
       ref={ref}
       sx={{
         px: { xs: '1.6rem', sm: '3.2rem', lg: '16.5rem', tv: '25rem' },
@@ -59,47 +61,51 @@ const Hero = React.forwardRef((_, ref) => {
         background:
           'linear-gradient(to bottom, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), center / cover no-repeat url(/me.jpg)',
         minHeight: '100vh',
-        color: theme => theme.palette.common.white,
+        color: (theme) => theme.palette.common.white,
         position: 'relative',
-      }}>
+      }}
+    >
       <PatternRingsRight />
       <PatternRingsLeft />
       <Typography
-        variant='h1'
+        variant="h1"
         sx={{
           mt: '9.6rem',
           mb: { sm: '4.8rem' },
-        }}>
-        <Typography component='span' variant='h3'>
+        }}
+      >
+        <Typography component="span" variant="h3">
           {t('greeting')}
         </Typography>
         <br />
         <Box
-          component='span'
+          component="span"
           sx={{
             position: 'relative',
             '::after': {
               content: "''",
               height: '0.4rem',
               width: '97%',
-              background: theme => theme.palette.primary.light,
+              background: (theme) => theme.palette.primary.light,
               position: 'absolute',
               bottom: '0.25rem',
               left: 0,
               borderRadius: '0.4rem',
             },
-          }}>
+          }}
+        >
           Juan Hamilton.
         </Box>
       </Typography>
       <Typography
-        variant='body1'
+        variant="body1"
         sx={{
           my: { xs: '2.4rem', sm: 0 },
           mb: { sm: '6.4rem' },
           textAlign: { xs: 'center', sm: 'left' },
           width: { sm: '44.5rem', desktop: '54.7rem', tv: '65rem' },
-        }}>
+        }}
+      >
         {t('hero-description')}
       </Typography>
       <Stack
@@ -107,21 +113,23 @@ const Hero = React.forwardRef((_, ref) => {
           alignItems: 'center',
           flexDirection: { xs: 'column', sm: 'row' },
           gap: { xs: '1.6rem', sm: '3.2rem' },
-        }}>
-        <ElevatedButton href='#contact'>{t('contact-me')}</ElevatedButton>
+        }}
+      >
+        <ElevatedButton href="#contact">{t('contact-me')}</ElevatedButton>
         <TextButton
           href={resumeLink}
-          target='_blank'
+          target="_blank"
           sx={{
             mb: { sm: '1rem' },
-          }}>
+          }}
+        >
           {t('view-resume')}
         </TextButton>
       </Stack>
     </Stack>
-  )
-})
+  );
+});
 
-Hero.displayName = 'Hero'
+Hero.displayName = 'Hero';
 
-export default Hero
+export default Hero;
